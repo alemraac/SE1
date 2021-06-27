48e39b2cd3ffd1775fae113fbf77fac83a8d9882
alemraac <70370573+alemraac@users.noreply.github.com>
2021-06-27 21:09:18 +0800

    added script7.js

---
+++ script7.js
@@ -0,0 +1,14 @@
+let numbers = [4,6,2,9,5];
+
+this.product = product;
+this.num = num;
+
+
+funtion getProduct(product,num){
+    return product*= num;
+
+}
+
+let productNums = number.reduce(getProduct);
+
+console.log(productNums);
